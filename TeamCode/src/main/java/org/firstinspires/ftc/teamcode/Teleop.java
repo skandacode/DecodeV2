14e2531e95b55b85ede00c07120fed34b468d360
skandacode <39654761+skandacode@users.noreply.github.com>
2026-01-11 17:39:16 -0600

    single intake teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,168 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.pedroPathing.Constants.createFollower;
+
+import com.bylazar.telemetry.JoinedTelemetry;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.sfdev.assembly.state.StateMachine;
+import com.sfdev.assembly.state.StateMachineBuilder;
+
+import org.firstinspires.ftc.teamcode.subsystems.Intakes;
+import org.firstinspires.ftc.teamcode.subsystems.Position;
+import org.firstinspires.ftc.teamcode.subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.subsystems.Spindexer;
+
+import java.util.Arrays;
+import java.util.List;
+
+import solverslib.gamepad.GamepadEx;
+import solverslib.gamepad.GamepadKeys;
+
+public class Teleop extends LinearOpMode {
+    Intakes intakes;
+    Spindexer spindexer;
+    Shooter shooter;
+    Follower follower;
+
+    public static Shooter.Goal target = Shooter.Goal.BLUE;
+    public static double powerOffsetIncrements = 20;
+    public static double turretOffsetIncrements = 2;
+
+    public enum States{
+        Intake,
+        Intake2,
+        OpenUpperGate,
+        Shoot,
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new JoinedTelemetry(telemetry, PanelsTelemetry.INSTANCE.getFtcTelemetry());
+
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+        LynxModule controlhub = null;
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+            if (hub.isParent()) {
+                controlhub = hub;
+                break;
+            }
+        }
+
+        GamepadEx gamepadEx = new GamepadEx(gamepad1);
+
+        intakes = new Intakes(hardwareMap);
+        spindexer = new Spindexer(hardwareMap);
+        shooter = new Shooter(hardwareMap);
+        follower = createFollower(hardwareMap);
+
+        GamepadKeys.Button slowModeButton = GamepadKeys.Button.RIGHT_BUMPER;
+        GamepadKeys.Button positionResetButton = GamepadKeys.Button.LEFT_BUMPER;
+        GamepadKeys.Button shooterButton = GamepadKeys.Button.B;
+
+        follower.setStartingPose(Position.pose);
+
+        StateMachine stateMachine = new StateMachineBuilder()
+                .state(States.Intake)
+                .onEnter(()->{
+                    intakes.setGoodIntakePower(1);
+                    intakes.setBadIntakeMotor(0.2);
+                    shooter.setUpperGatePos(false);
+                    spindexer.setLowerGatePos(true);
+                    spindexer.setKickerPos(false);
+                    spindexer.setPosition(Spindexer.SpindexerPosition.Shoot1);
+                })
+                .transition(()->gamepadEx.getButton(shooterButton), States.OpenUpperGate)
+                .state(States.OpenUpperGate)
+                .onEnter(()->{
+                    shooter.setUpperGatePos(true);
+                })
+                .transitionTimed(0.1, States.Shoot)
+                .state(States.Shoot)
+                .onEnter(()->{
+                    spindexer.setKickerPos(true);
+                })
+                .transitionTimed(0.3, States.Intake)
+                .build();
+
+        while (opModeInInit()) {
+            for (LynxModule hub : allHubs) hub.clearBulkCache();
+            follower.update();
+            if (gamepad1.a) {
+                target = Shooter.Goal.BLUE;
+            }
+            if (gamepad1.b) {
+                target = Shooter.Goal.RED;
+            }
+            telemetry.addData("Shooter Target", target);
+            telemetry.addData("Current Pos", follower.getPose());
+            telemetry.update();
+        }
+
+        waitForStart();
+
+        long lastLoopTime = System.nanoTime();
+        while (opModeIsActive()) {
+            if (controlhub != null) {
+                controlhub.clearBulkCache();
+            }else {
+                for (LynxModule hub : allHubs) {
+                    hub.clearBulkCache();
+                }
+            }
+
+            gamepadEx.readButtons();
+
+            follower.update();
+            Position.pose = follower.getPose();
+            telemetry.addData("Angle and distance:", Arrays.toString(shooter.getAngleDistance(Position.pose, target)));
+            shooter.aimAtTarget(Position.pose, target);
+
+            double forward = gamepadEx.getLeftY();
+            double strafe = gamepadEx.getLeftX();
+            double turn = gamepadEx.getRightX();
+            if (gamepadEx.getButton(slowModeButton)) {
+                forward *= 0.3;
+                strafe *= 0.3;
+                turn *= 0.3;
+            }
+
+            follower.setTeleOpDrive(forward, -1*strafe, -1*turn, true);
+
+            if (gamepadEx.wasJustPressed(positionResetButton)){
+                follower.setPose(new Pose(65, 0, Math.toRadians(180)));
+            }
+            if (gamepadEx.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)){
+                Shooter.powerOffset -= powerOffsetIncrements;
+            }
+            if (gamepadEx.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
+                Shooter.turretOffset -= turretOffsetIncrements;
+            }
+            if (gamepadEx.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)){
+                Shooter.turretOffset += turretOffsetIncrements;
+            }
+            if (gamepadEx.wasJustPressed(GamepadKeys.Button.DPAD_UP)){
+                Shooter.powerOffset += powerOffsetIncrements;
+            }
+
+
+            telemetry.addData("Current Pos", follower.getPose());
+            telemetry.addData("Shooter Target", shooter.getTargetVelo());
+            telemetry.addData("Shooter Velocity", shooter.getCurrentVelocity());
+            telemetry.addData("Turret Voltage", shooter.getTurretVoltage());
+
+            long currentTime = System.nanoTime();
+            double loopTime = (double) (currentTime - lastLoopTime) / 1_000_000.0;
+            lastLoopTime = currentTime;
+            telemetry.addData("Loop time", loopTime);
+            intakes.update();
+            spindexer.update();
+            shooter.update();
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Position.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.pedropathing.geometry.Pose;
+
+public class Position {
+    public static Pose pose = new Pose();
+}
