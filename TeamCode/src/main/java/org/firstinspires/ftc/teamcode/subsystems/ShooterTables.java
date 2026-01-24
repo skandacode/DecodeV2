4e39304831deee349e528ee63d85ff42e5e6499f
skandacode <39654761+skandacode@users.noreply.github.com>
2026-01-24 11:08:40 -0600

    update shooter regressions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterTables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterTables.java
@@ -3,18 +3,18 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 public class ShooterTables {
     public static double getHoodPosition(double distance) {
-        return -1.03121e-8 * Math.pow(distance, 4)
-                + 0.00000451516 * Math.pow(distance, 3)
-                - 0.000725976   * Math.pow(distance, 2)
-                + 0.0517744     * distance
-                - 0.793225;
+        return -1.34068e-8 * Math.pow(distance, 4)
+                + 0.00000533003 * Math.pow(distance, 3)
+                - 0.000773036   * Math.pow(distance, 2)
+                + 0.051215    * distance
+                - 0.73333;
     }
     public static double getShooterVelocity(double distance) {
-            return -0.0000131929 * Math.pow(distance, 4)
-                    + 0.00548226 * Math.pow(distance, 3)
-                    - 0.807222 * Math.pow(distance, 2)
-                    + 56.34443 * distance
-                    - 67.60176;
+            return -0.00000527297 * Math.pow(distance, 4)
+                    + 0.00184391 * Math.pow(distance, 3)
+                    -0.227349 * Math.pow(distance, 2)
+                    + 19.86808 * distance
+                    - 697.3213;
 
     }
 
