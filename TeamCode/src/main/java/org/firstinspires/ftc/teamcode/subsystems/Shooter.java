cf6a38344b9643e5b53d5819a1c31cec401a64b7
skandacode <39654761+skandacode@users.noreply.github.com>
2026-01-26 11:28:47 -0600

    remove normalization because it slows us down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -170,8 +170,6 @@ public class Shooter {
             }
         }
 
-        outputPower = Math.max(-1, Math.min(1, outputPower));
-
         setDirectPower(outputPower);
         shooterMotor1.update();
         shooterMotor2.update();
