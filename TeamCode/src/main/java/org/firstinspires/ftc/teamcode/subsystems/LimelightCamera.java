45454d886e0b46d243e59e4598f7f5f928443b5d
skandacode <39654761+skandacode@users.noreply.github.com>
2026-04-19 21:08:23 -0500

    Update LimelightCamera.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightCamera.java
@@ -11,22 +11,10 @@ public class LimelightCamera {
 
     private Limelight3A limelight;
 
-    public enum Pipelines{
-        MOTIF (0),
-        BALLTRACKING (1);
-
-        public final int index;
-
-        Pipelines(int index) {
-            this.index = index;
-        }
-    }
-
-    private Pipelines currentPipeline = Pipelines.MOTIF;
 
     public LimelightCamera(HardwareMap hardwareMap){
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(currentPipeline.index);
+        limelight.pipelineSwitch(0);
         start();
     }
 
@@ -38,56 +26,33 @@ public class LimelightCamera {
         limelight.stop();
     }
 
-    public void setCurrentPipeline(Pipelines pipeline){
-        limelight.pipelineSwitch(pipeline.index);
-        currentPipeline = pipeline;
-    }
-
     public int getMotif(){
-        if (currentPipeline == Pipelines.MOTIF) {
-            LLResult result = limelight.getLatestResult();
-            int pattern = 0;
-            if (result != null && result.isValid()) {
-                List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
-                for (LLResultTypes.FiducialResult fr : fiducialResults) {
-                    if (fr.getFiducialId() > 20 && fr.getFiducialId() < 24) {
-                        pattern = fr.getFiducialId() - 20;
-                    }
+        LLResult result = limelight.getLatestResult();
+        int pattern = 0;
+        if (result != null && result.isValid()) {
+            List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+            for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                if (fr.getFiducialId() > 20 && fr.getFiducialId() < 24) {
+                    pattern = fr.getFiducialId() - 20;
                 }
             }
-            return pattern; //returns 0 if it doesn't see anything
-        }else{
-            return -1;
         }
+        return pattern; //returns 0 if it doesn't see anything
     }
-    public LLFieldScannerResults getTrackingResults(){
-        if (currentPipeline == Pipelines.BALLTRACKING){
-            limelight.updatePythonInputs(new double[] {0, 0, 0, 0, 0, 0, 0, 0});
-            LLResult result = limelight.getLatestResult();
-            if (result != null) {
-                System.out.println(result.getTx()+"    "+result.getTy());
-                double[] output = result.getPythonOutput();
-                if (output == null){
-                    System.out.println("Output is null");
-                    return null;
-                }
-                if (output.length == 0) {
-                    System.out.println("length is 0");
-                    return null;
-                }
-                if (output[1] == 1.0) {
-                    return new LLFieldScannerResults(result.getTx(), result.getTy());
-                }else{
-                    return null;
+    public double getTrackingResults(boolean isBlueAlliance){
+        double targetId = 24;
+        if (isBlueAlliance){
+            targetId = 20;
+        }
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+            for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                if (fr.getFiducialId() == targetId) {
+                    return result.getTx();
                 }
             }
-            System.out.println("Not valid");
         }
-        System.out.println("returning null");
-        return null;
-    }
-
-    public Pipelines getCurrentPipeline(){
-        return currentPipeline;
+        return 0;
     }
 }
